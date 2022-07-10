403bbb2ae2a4723f1d8008e20400026b6ffe0bae
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-07-10 21:49:11 +0800

    xommitting new proj

---
+++ src/main/java/guru/springframework/nish/jokes/NishJokesApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.nish.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NishJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NishJokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/nish/jokes/NishJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.nish.jokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NishJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
