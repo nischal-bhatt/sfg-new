ce4d5c635eaea99ae016c8653afbf0c15c10f6af
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-07-11 20:44:45 +0800

    committing some properties stuff

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -9,7 +9,9 @@ import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.*;
+@EnableConfigurationProperties(SfgConstructorConfig.class)
 @PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
