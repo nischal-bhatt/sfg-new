1d5836a94a3907321dd0d2c7b58eeffeb577b3e3
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-07-11 20:37:04 +0800

    committing some properties stuff

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.config.SfgConfiguration;
 import guru.springframework.sfgdi.controllers.*;
 import guru.springframework.sfgdi.datasource.FakeDataSource;
 import guru.springframework.sfgdi.services.PrototypeBean;
@@ -55,6 +56,14 @@ public class SfgDiApplication {
 		System.out.println(fakeDataSource.getUsername());
 		System.out.println(fakeDataSource.getPassword());
 		System.out.println(fakeDataSource.getJdbcurl());
+
+		System.out.println("----- config props bean");
+
+		SfgConfiguration sfgConfiguration
+				= (SfgConfiguration)ctx.getBean("sfgConfiguration");
+
+		System.out.println(sfgConfiguration.getUsername());
+
 
 	}
 
