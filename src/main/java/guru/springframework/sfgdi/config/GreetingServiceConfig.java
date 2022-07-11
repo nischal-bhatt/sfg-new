7477455e9cf3f535160c7092a58c3e49b01e4241
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-07-11 10:07:05 +0800

    more config stuff

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,5 +1,8 @@
 package guru.springframework.sfgdi.config;
 
+import com.springframework.pets.DogPetService;
+import com.springframework.pets.PetService;
+import com.springframework.pets.PetServiceFactory;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
@@ -11,6 +14,26 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Bean
+    PetServiceFactory petServiceFactory()
+    {
+        return new PetServiceFactory();
+    }
+
+    @Profile({"dog","default"})
+    @Bean
+    PetService dogPetService(PetServiceFactory petServiceFactory)
+    {
+        return  petServiceFactory.getPetService("dog");
+    }
+
+    @Profile("cat")
+    @Bean
+    PetService catPetService(PetServiceFactory petServiceFactory)
+    {
+        return petServiceFactory.getPetService("cat");
+    }
 
     @Bean
     EnglishGreetingRepository englishGreetingRepository()
